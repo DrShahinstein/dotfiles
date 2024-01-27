f15a8a41edee68ceb590c6994a0bc9ef768376a8
DrShahinstein <1taylanozturk@protonmail.com>
2024-01-27 12:36:45 +0300

    Update neovim config

--- .config/nvim/custom/configs/formatter.lua
+++ .config/nvim/custom/configs/formatter.lua
@@ -1,21 +1,21 @@
 local conform = require "conform"
 
 local options = {
-	lsp_fallback = true,
-	formatters_by_ft = {
+  lsp_fallback = true,
+  formatters_by_ft = {
     c = { "clangd-format" },
     cpp = { "clangd-format" },
     python = { "autopep8" },
-		sh = { "shfmt" },
-		html = { "djlint" },
-  	css = { "prettier" },
-	  javascript = { "prettier" },
-	  lua = { "stylua" },
+    sh = { "shfmt" },
+    html = { "djlint" },
+    css = { "prettier" },
+    javascript = { "prettier" },
+    lua = { "stylua" },
     json = { "prettier" },
-	},
+  },
 }
 
-vim.keymap.set({ 'n' }, "<leader>F", function ()
+vim.keymap.set({ 'n' }, "<leader>F", function()
   conform.format({
     lsp_fallback = true,
     async = false,
@@ -24,4 +24,3 @@ vim.keymap.set({ 'n' }, "<leader>F", function ()
 end, { desc = "Format file" })
 
 conform.setup(options)
-
