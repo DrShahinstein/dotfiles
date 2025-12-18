3472b29ed61fe2d438d336034af0581c706b0cb3
DrShahinstein <1taylanozturk@protonmail.com>
2025-12-18 18:11:23 +0300

    Delete obsolete nvchad configs for neovim

--- .config/nvim/custom/init.lua
+++
@@ -1,7 +0,0 @@
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = false,
-})
-
-vim.wo.relativenumber = true
-
-vim.keymap.set("v", "<Leader>f", vim.lsp.buf.format, {})
